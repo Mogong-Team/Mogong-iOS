a25e8f02bda692277cd21a096e8009cfa2006a36
hsuuuk <hsuuuk.ios@gmail.com>
2023-06-14 13:11:13 +0900

    Initial Commit

---
+++ Mogong/MogongApp.swift
@@ -0,0 +1,17 @@
+//
+//  MogongApp.swift
+//  Mogong
+//
+//  Created by 심현석 on 2023/06/14.
+//
+
+import SwiftUI
+
+@main
+struct MogongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
