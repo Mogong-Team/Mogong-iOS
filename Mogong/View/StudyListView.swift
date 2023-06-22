d531d49be6aee5103ab4ba4eabc3fa0412afdfe6
hsuuuk <hsuuuk.ios@gmail.com>
2023-06-22 09:46:37 +0900

    Implement studylist navigationview, searchbar

--- Mogong/View/StudyListView.swift
+++ Mogong/View/StudyListView.swift
@@ -8,14 +8,87 @@
 import SwiftUI
 
 struct StudyListView: View {
+    @EnvironmentObject var viewModel: StudyViewModel
+    
+    var items = ["1", "2", "3", "4", "5"]
+    
     var body: some View {
-        Text("하이")
-    }
+            VStack {
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Text("인기순")
+                            .padding([.leading, .trailing], 10)
+                            .padding([.top, .bottom], 5)
+                            .background(Color.gray)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(15)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("최신순")
+                            .padding([.leading, .trailing], 10)
+                            .padding([.top, .bottom], 5)
+                            .background(Color.gray)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(15)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("포인트순")
+                            .padding([.leading, .trailing], 10)
+                            .padding([.top, .bottom], 5)
+                            .background(Color.gray)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(15)
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 10)
+                
+                ScrollView {
+                    ForEach(items, id:\.self) { item in
+                        StudyListCell()
+                            .background(Color(uiColor: .systemGray5))
+                            .padding(.horizontal, 30)
+                            .cornerRadius(30)
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Text("Mogong")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                }
+                
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    NavigationLink {
+
+                    } label: {
+                        Image(systemName: "plus")
+                            .tint(.black)
+                    }
+                }
+            }
+            .searchable(text: $viewModel.searchQuery, prompt: "스터디 검색하기") {
+                
+            }
+        }
 }
 
 struct StudyListView_Previews: PreviewProvider {
     static var previews: some View {
-        StudyListView()
+        NavigationView {
+            StudyListView()
+                .environmentObject(StudyViewModel())
+        }
     }
 }
 
