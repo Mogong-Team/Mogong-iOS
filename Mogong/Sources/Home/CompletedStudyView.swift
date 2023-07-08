f9e06c785b2053d93a5b62b5a1a5778cb2e99418
ye-gang-jjang <yegang13209@naver.com>
2023-07-08 19:29:25 +0900

    Implement CompletedStudyView

--- Mogong/Sources/Home/CompletedStudyView.swift
+++ Mogong/Sources/Home/CompletedStudyView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct CompletedStudyView: View {
+    @EnvironmentObject var viewModel: StudyViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 8) {
+            Text("방금 완주한 스터디")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("방금 완주한 따끈따끈한 스터디 목록을 살펴보세요!")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 16) {
+                    ForEach(viewModel.studys) { study in
+                        CompletedStudyCell(study: study)
+                    }
+                }
+            }
+        }
+//        .padding(.top, 30)
+        .padding()
     }
 }
 
 struct CompletedStudyView_Previews: PreviewProvider {
     static var previews: some View {
         CompletedStudyView()
+            .environmentObject(StudyViewModel())
     }
 }
