b2b049bb01d513e94de050b13cff9b60f9050f53
ye-gang-jjang <yegang13209@naver.com>
2023-07-08 19:30:30 +0900

    Implement ClippyStudyView

--- Mogong/Sources/Home/ClippyStudyView.swift
+++ Mogong/Sources/Home/ClippyStudyView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ClippyStudyView: View {
+    @EnvironmentObject var viewModel: StudyViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("스크랩이 많은 스터디")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text("관심 집중 스터디에요!")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+
+            ScrollView() {
+                VStack(spacing: 16) {
+                    ForEach(viewModel.studys) { study in
+                        ClippyStudyCell(study: study)
+                    }
+                }
+            }
+        }
+        .padding()
     }
 }
 
 struct ClippyStudyView_Previews: PreviewProvider {
     static var previews: some View {
         ClippyStudyView()
+            .environmentObject(StudyViewModel())
     }
 }
