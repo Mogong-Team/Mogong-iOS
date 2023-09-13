2e6176580499dd6d38fd35733056b0292cfcba22
ye-gang-jjang <yegang13209@naver.com>
2023-09-13 19:48:25 +0900

    refactor 소셜로그인 수정

--- Mogong/Sources/View/Authentication/SocialLogin.swift
+++ Mogong/Sources/View/Authentication/SocialLogin.swift
@@ -18,41 +18,24 @@ struct SocialLogin: View {
             Button {
                 viewModel.loginWithKakaoTalk()
             } label: {
-                VStack {
-                    Image("kakaoLogo")
-                        .frame(width: 73, height: 73)
-                    Text("카카오로\n시작하기")
-                        .font(.pretendard(weight: .medium, size: 14))
-                        .foregroundColor(Color(hexColor: "494949"))
-                }
+                Image("kakaoLogo")
+                    .frame(width: 73, height: 73)
             }
-            
             // MARK: - 구글 로그인
             
             Button {
                 viewModel.loginWithGoogle()
             } label: {
-                VStack {
-                    Image("googleLogo")
-                        .frame(width: 73, height: 73)
-                    Text("구글로\n시작하기")
-                        .font(.pretendard(weight: .medium, size: 14))
-                        .foregroundColor(Color(hexColor: "494949"))
-                }
+                Image("googleLogo")
+                    .frame(width: 73, height: 73)
             }
-            
             // MARK: - 애플 로그인
             
             Button {
                 viewModel.loginWithApple()
             } label: {
-                VStack {
-                    Image("appleLogo")
-                        .frame(width: 73, height: 73)
-                    Text("애플로\n시작하기")
-                        .font(.pretendard(weight: .medium, size: 14))
-                        .foregroundColor(Color(hexColor: "494949"))
-                }
+                Image("appleLogo")
+                    .frame(width: 73, height: 73)
             }
             
             // MARK: - 네이버 로그인
